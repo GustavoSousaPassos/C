abf5ff5331cb6c5a5b00a8d09deaa41f6233f679
Gustavo Sousa Passos <39065441+GustavoSousaPassos@users.noreply.github.com>
2023-07-15 14:49:26 -0300

    Update bubble_sort.c

--- c/bubble_sort.c
+++ c/bubble_sort.c
@@ -1,33 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int v[5], i, j, aux = 0, flag; //63724
+	int vec[5], i, aux = 0, flag;
 	
-	v[0] = 6;
-	v[1] = 3;
-	v[2] = 7;
-	v[3] = 2;
-	v[4] = 4;
+	vec[0] = 6;
+	vec[1] = 3;
+	vec[2] = 7;
+	vec[3] = 2;
+	vec[4] = 4;
 	
 	do
 	{
 		flag = 0;
-		for(i = 0; i <= 4; i++)
+		
+		for(i = 0; i < 5; i++)
 		{
-				if (v[i] > v[i+1])
-				{
-					aux = v[i];
-					v[i] = v[i+1];
-					v[i+1] = aux;
-					flag = 1;
-				}
+			if(vec[i] > vec[i+1])
+			{
+				aux = vec[i];
+				vec[i] = vec[i+1];
+				vec[i+1] = aux;
+				
+				flag = 1;
+			}
 		}
 	} while(flag == 1);
-	for(i=0;i<5;i++)
-	{
-		printf("\n \n v[%d] = %d \n", i, v[i]);
-	}
 	
+	for(i = 0; i < 5; i++)
+		printf("\n \n vec[%d] = %d\n", i, vec[i]);
 }
-	
